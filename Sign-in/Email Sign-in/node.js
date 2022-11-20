75eb9cc81ae066a7d63b87942cc8a8029f70221c
Goutham2010 <116776472+Goutham2010@users.noreply.github.com>
2022-11-20 17:15:52 +0530

    Update node.js

--- Sign-in/Email Sign-in/node.js
+++ Sign-in/Email Sign-in/node.js
@@ -1,1 +1,55 @@
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAxGOKyUhXDrNs54bGMN0waCFOGLnBl68Q",
+    authDomain: "friends-club-7582f.firebaseapp.com",
+    databaseURL: "https://friends-club-7582f-default-rtdb.firebaseio.com",
+    projectId: "friends-club-7582f",
+    storageBucket: "friends-club-7582f.appspot.com",
+    messagingSenderId: "324331195024",
+    appId: "1:324331195024:web:146d81b214249051f62615",
+    measurementId: "G-94NW6VJDQJ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
+  const auth =  firebase.auth();
+
+  //signup function
+  function signUp(){
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+
+    const promise = auth.createUserWithEmailAndPassword(email.value,password.value);
+    //
+    promise.catch(e=>alert(e.message));
+    alert("SignUp Successfully");
+  }
+
+  //signIN function
+  function  signIn(){
+    var email = document.getElementById("email");
+    var password  = document.getElementById("password");
+    const promise = auth.signInWithEmailAndPassword(email.value,password.value);
+    promise.catch(e=>alert(e.message));
+    
+  }
+
+
+  //signOut
+
+  function signOut(){
+    auth.signOut();
+    alert("SignOut Successfully from System");
+  }
+
+  //active user to homepage
+  firebase.auth().onAuthStateChanged((user)=>{
+    if(user){
+      var email = user.email;
+      alert("Active user "+email);
+
+    }else{
+      alert("No Active user Found")
+    }
+  })
